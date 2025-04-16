27fda7037890bc83c2c144b1211e5ba9649d5497
Hotocrypto <hotocrypto@proton.me>
2025-04-16 11:56:30 -0700

    Update

--- utils/claimButton.js
+++ utils/claimButton.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import { ClaimButton } from "thirdweb/react";
-import { baseSepolia , base } from "thirdweb/chains";
+import { baseSepolia, base } from "thirdweb/chains";
 import { createThirdwebClient } from "thirdweb";
 import { lightTheme } from "thirdweb/react";
 
 export default function MintButton({ quantity, onTransactionConfirmed }) {
+  console.log('quantity', quantity);
 
-  console.log('quantity',quantity)
-    const client = createThirdwebClient({
-        clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-    });
-    return (
-        <ClaimButton
-            contractAddress={process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}
-            chain={base}
-            client={client}
-            claimParams={{
-                type: "ERC721",
-                quantity: 1n,
-            }}
-            theme={lightTheme({
-                colors: {
-                    primaryButtonBg: "#c9f364",
-                    primaryButtonText: "#000000",
-                },
-            })}
-            onTransactionConfirmed={(receipt) => {
-                console.log("Transaction confirmed", receipt.transactionHash);
-                if (onTransactionConfirmed) {
-                    onTransactionConfirmed(receipt.transactionHash);
-                }
-            }}
-            style={{
-              width:'130px',
-              height:'50px',
-              fontSize:'20px'
-            }}
-        >
-            Mint Now
-        </ClaimButton>
-    );
+  const client = createThirdwebClient({
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  });
+
+  return (
+    <ClaimButton
+      contractAddress={process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}
+      chain={base}
+      client={client}
+      claimParams={{
+        type: "ERC721",
+        quantity: BigInt(quantity), // Use the passed quantity, converted to bigint
+      }}
+      theme={lightTheme({
+        colors: {
+          primaryButtonBg: "#c9f364",
+          primaryButtonText: "#000000",
+        },
+      })}
+      onTransactionConfirmed={(receipt) => {
+        console.log("Transaction confirmed", receipt.transactionHash);
+        if (onTransactionConfirmed) {
+          onTransactionConfirmed(receipt.transactionHash);
+        }
+      }}
+      style={{
+        width: '130px',
+        height: '50px',
+        fontSize: '20px',
+      }}
+    >
+      Mint Now
+    </ClaimButton>
+  );
 }
